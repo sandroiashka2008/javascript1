7120e6f6ce1ba5cc8c2fbecab80cd4983682619e
sandro antadze <sandroiashvili169@gmail.com>
2024-10-18 15:49:19 +0400

    javascript1

---
+++ script.js
@@ -0,0 +1,28 @@
+const firstname = 'sandro'
+console.log(firstname)
+
+
+const lastname = 'antadze/iashvili'
+console.log(lastname)
+
+
+const text = 'sandroiashvili169@gmail.com'
+console.log(text)
+
+let number = 599609036
+console.log('%c' + number, 'color: red;');
+
+
+
+ 
+//ასაკი
+
+ const numb = 16
+
+console.log(numb);
+
+//booelan
+
+const isAdult  = false;
+
+console.log('%c' + isAdult, 'color: yellow;');
